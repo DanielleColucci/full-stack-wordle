37ad48df205db4de4c959e5eca61c7908e34c9fa
DanielleColucci <ddcolucci99@gmail.com>
2023-04-05 11:56:19 -0400

    create Score model

---
+++ models/score.js
@@ -0,0 +1,43 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Score extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Score.init({
+    mode: {
+      type: DataTypes.ENUM(1, 2, 4, 8, 16),
+      defaultValue: 1,
+    },
+    value: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 6,
+      }
+    },
+    profileId: {
+      types: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Profiles',
+        key: 'id',
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Score',
+  });
+  return Score;
+};
