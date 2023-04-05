1f08a0c1af5dbc8a8ffeb677decb475eff1ef36b
DanielleColucci <ddcolucci99@gmail.com>
2023-04-05 14:28:09 -0400

    define and export create controller function for scores

--- controllers/scores.js
+++ controllers/scores.js
@@ -1,6 +1,15 @@
 const { Score } = require('../models')
 
+const create = async (req, res) => {
+  try {
+    req.body.profileId = req.user.profile.id
+    const score = await Score.create(req.body)
+    res.status(200).json(score)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
 
 module.exports = {
-  
+  create,
 }
